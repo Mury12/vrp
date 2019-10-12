bcc40f567918227cc4ead9b5c808b1070e8d65ac
André Mury <mury_gh@hotmail.com>
2019-10-11 21:09:00 -0300

    novo solver

--- classes/model/Depot.py
+++ classes/model/Depot.py
@@ -10,30 +10,25 @@ true  = True
 
 class Depot:
     
-    pos = []
-
-    vehicles = []
-    # Usada para calcular as rotas
-    customers = []
-    # Usada para reservar os clientes
     _customers = []
-    _distMatrix = []
     timeConst = 0
     timeRef1 = 0
+
     def __init__(self, pos, vehicles, cap):
         
         self.unloaded = false
         self.pos = pos
         self.vehicles = self.createVehicles(int(vehicles), int(cap))
+        self.distMatrix = []
+        self.customers = []
+        self._distMatrix = []
     
     def createVehicles(self, amount, cap):
-        #print("Creating vehicles.. \n")
         vehicles = []
 
         for i in range (0, amount):
             vehicles.append(Vehicle(i, cap, self.pos))
         
-        #print(str(amount)+" Vehicles created.\n")
         return vehicles
 
 
@@ -42,7 +37,6 @@ class Depot:
         self._customers.append(customer)
 
     def bulkAddCustomer(self, dataset):
-        #print("Bulk adding customers..\n")
         i = 0
         for item in dataset:
             pos = Point2D(float(item[0]), float(item[1]))
@@ -50,165 +44,15 @@ class Depot:
             self.addCustomer(Customer(i, pos, demand))
             i = i+1
 
-        #print(str(i)+" Customers added. \n")
         self.customers[0].unload()
         self._createDistancMatrix()
 
     def removeCustomer(self, customer):
         return false
 
-    def traceRoutes(self, ref):
-        #print("Tracing routes..")
-        i = 0
-        #k = k if k > 0 else k + 1
-        for i in range (0, self._distMatrix[i].__len__()):
-        # for i in range (0, 1):
-            for v in self.vehicles:
-                _next = self._getMinorDistanceIndex(v.pos,4)
-                if(v.addCustomer(self.customers[_next[0]], _next[1])):
-                    self.customers[_next[0]].unload()
-
-               
-        #print(self.vehicles)
-        #print("Routes traced.\n")
-
-        distanceParcial = 0
-        for v in self.vehicles:
-            distanceParcial += v.getTotalDistance() #soma a distancia percorrido pelo veiculo
-            #soma a distancia de volta do veiculo para o depot a partir do ultimo cliente
-            distanceVolta = v.route[-1][0].pos.distanceTo(Point2D(float(self.pos.x),float(self.pos.y)))
-            distanceParcial += distanceVolta
-        #print("\nDistância total da solução: \n" + str(distanceParcial) + '\n')
-        #print("Tempo de execução com a heurística construtiva: " + str(self.timeConst) + '\n')
-        #se for escolhido usar o método de refinamento de trocas...
-
-        #verifica se existe uma solução para o problema, caso n exista o k é iterado e o programa 
-        #buscará por uma nova solução
-        #isDone = self.reportLoadedUnloaded()
-        #if(not isDone):
-        #    self.traceRoutes(k)
-
-        if(ref == 1):
-            #print("entrou no refinamento")
-            self._changeRefine(4) #alterar aq o envio da optimization rate and seed (seria bom um dps 
-            #colocarmos um input())
-            distanceParcial = 0
-            for v in self.vehicles:
-                distanceParcial += v.getTotalDistance() #soma a distancia percorrido pelo veiculo
-                #soma a distancia de volta do veiculo para o depot a partir do ultimo cliente 
-                distanceVolta = v.route[-1][0].pos.distanceTo(Point2D(float(self.pos.x),float(self.pos.y)))
-                distanceParcial += distanceVolta
-            '''print("\nNew routes traceds...\n")
-            print(self.vehicles)
-            print("\nRoutes traced.")'''
-            print(str(distanceParcial))
-            #print("Tempo de execução com a heurística refinamento: " + str(self.timeRef1) +'\n')
-
-        return false
-        
-    def _getMinorDistanceIndex(self, curPos, k):
-        timeStart = time.time()
-        _next = 9999
-        _idx = 0
-        i = 0
-        testRow = []
-        #altera a posição de um cliente
-        #objetivo é alterar a posição um específico cliente caso n encontre uma solução na tentativa anterior
-        if(k != 0 and k <self._distMatrix[curPos].__len__()):
-            row = []
-            row = list(self._distMatrix[curPos])
-            val = self._distMatrix[curPos][k]
-            row.insert(0, val) #reposiciona o cliente na primeira posição do array
-            row.__delitem__(k+1) #remove o cliente da posição q o cliente estava por padrão
-            testRow = row
-            #print("linha" + str(row))
-            #print("valor " + str(k), "é : " + str(val))
-        for d in testRow:
-            if(d > 0 and d < _next and self.customers[i].loaded):
-                _next = d
-                _idx = i
-            i += 1
-
-        _result = []
-        _result.append(_idx)
-        _result.append(_next)
-
-        timeEnd = time.time()
-        self.timeConst += timeEnd - timeStart
-        
-        return _result
-
-    def _randomizeResult(self, curPos, seed):
-        random.seed(seed)
-        _idx = _next = 0
-        while((_idx == 0 or _next == -1 or _next == 0) and not self.customers[_idx].loaded):
-            _idx = int(random.randint(1, self._distMatrix[_idx].__len__() - 1))
-            _next = self._distMatrix[curPos][_idx]
-
-        _result = []
-        _result.append(_idx)
-        _result.append(_next)
-
-        return _result
-
-    def _changeRefine(self, ot):
-        timeStart = time.time() #time start do método de refinamento
-        i = 0 #Optimization rates = Quantidade de veículos q serão refinados (os veículos q percorrem maior distância)
-        _midx = [] #list q armazena o indice real dos veículos em self.vehicles
-        _idx = 0 #indice q identifica o veiculo q passou apresentar ter maior distancia percorrida (pior)
-        cloneVehicles = self.vehicles
-        worstRoutes = [] #list q armazena os piores veiculos
-        while i < ot: #para a qtde veiculos q deverá ser refeito
-            j = 0
-            _next = 0
-            for v in cloneVehicles:
-                #algoritmo para encontrar o pior veiculo
-                if v.distRan > _next:
-                    _next = v.distRan
-                    _idx = j
-                j += 1
-            _midx.append(_idx) #registro o indice do pior veiculo encontrado
-            worstRoutes.append(cloneVehicles[_idx]) #insere o pior veiculo na lista de piores veiculos
-            cloneVehicles[_idx].distRan = 0
-            i += 1
-        
-        for c in worstRoutes:
-            for w in range (0,int(c.route.__len__()/2)):
-                x = randint(0, c.route.__len__()-1)
-                customer1 = c.route[w]
-                customer2 = c.route[x]
-                c.route.insert(w, customer2)
-                c.route.__delitem__(w+1)
-                c.route.insert(x, customer1)
-                c.route.__delitem__(x+1)
-            c.distRan = self._recalcDistanc(c)
-        
-        sum = 0 #soma da distancia total com o refinamento das piores rotas
-        for c in worstRoutes:
-            #print(c.distRan)
-            sum += c.distRan
-        
-        #print("Soma da distância total das rotas que foram refinadas -> " + str(sum))
-        for i in range (0,_midx.__len__()):
-            self.vehicles[_midx[i]].distRan = worstRoutes[i].distRan #atualiza o distRan da pior rota 
-            #self.vehicles.insert(_midx[i], worstRoutes[i])
-            #self.vehicles.__delitem__(_midx[i]+1)
-
-        timeEnd = time.time() #calc tempod e execução do método de refinamento
-        self.timeRef1 = timeEnd - timeStart
-
-    def _recalcDistanc(self, vec):
-        newDistRan = 0 #distancia inicial do recalculo da rota
-        for c in range(0, vec.route.__len__() -1 ): #para cada customer da rota - o último
-            cActual = str(vec.route[c]).split('(')[1].split(')')[0].split(',') #obter os pontos do customer
-            cNext  = str(vec.route[c+1]).split('(')[1].split(')')[0].split(',') #obter os pontos do customer
-            pointActual = Point2D(float(cActual[0]), float(cActual[1]))
-            pointNext = Point2D(float(cNext[0]), float(cNext[1]))
-            newDistRan += self.distanceBetween(pointActual, pointNext)
-        return newDistRan
-
-    def distanceBetween(self, actual, next): #calcula a distancia entre dois pontos
-        return math.sqrt( (next.x - actual.x) ** 2 + (next.y - actual.y) ** 2 )
+    
+    def distanceBetween(self, current, next): #calcula a distancia entre dois pontos
+        return math.sqrt( (next.x - current.x) ** 2 + (next.y - current.y) ** 2 )
 
     def _createDistancMatrix(self):
         #print("Creating distance matrix..\n")
@@ -236,7 +80,7 @@ class Depot:
             print("\n")
 
     def reportLoadedUnloaded(self):
-        '''loaded = -1
+        loaded = -1
         unloaded = []
         for c in self.customers:
             loaded += 1 if not c.loaded else 0
@@ -250,7 +94,7 @@ class Depot:
             print(unloaded)
             self.unloaded = true
         else:
-            self.unloaded = false'''
+            self.unloaded = false
 
-        return self.vehicles
+        return self.unloaded
         
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import sys
 from classes.model.Parser import *
 from classes.model.Depot import *
-
+from classes.model.Solver import *
 parser = Parser()
 # parser.parse()
 i=0
@@ -30,11 +30,42 @@ depot = Depot(
 depot.bulkAddCustomer(dataset)
 
 # k and refineMethod
-isDone = depot.traceRoutes(1)
+# isDone = depot.traceRoutes(1)
 # print(depot.vehicles[0].route)
 # print(depot.vehicles[1].route)
+k = 0 
+global_optimal = 99999
+best_skip = 0
+for k in range (2, dataset.__len__()-1):
+    S = Solver(Depot (
+            Point2D(depotPos[0], depotPos[1]),
+            vehicles[0],
+            maxCap[0]
+    ), dataset)
 
-sol.append(depot.reportLoadedUnloaded())
+    S.traceRoutes(k)
+    # print("K = "+str(k)+ ' - '+ str(S.global_optimal))
+
+    if(S.global_optimal < global_optimal):
+        best_skip = k
+        global_optimal = S.global_optimal
+
+S = Solver(Depot (
+            Point2D(depotPos[0], depotPos[1]),
+            vehicles[0],
+            maxCap[0]
+    ), dataset)
+S.traceRoutes(best_skip)
+print('Best Skip: '+ str(best_skip))
+print('Best Solution: '+ str(S.global_optimal))
+S.depot.reportLoadedUnloaded()
+
+# for s in S.all_solutions:
+#     print(s)
+
+
+
+# sol.append(depot.reportLoadedUnloaded())
 '''
 if(not isDone):
     print("There is missing customers. Result needs to be optimized.\n")
